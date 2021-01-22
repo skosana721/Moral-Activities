a4279383f033949da2bf3035180f68e40bb768c3
TCG <wonganiskosana@gmail.com>
2021-01-22 15:58:25 +0200

    refactored code that is responsible for starting timer

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,40 +2,45 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [timer, setTimer] = useState(60);
+  const [timer, setTimer] = useState(10);
   const [isActive, setIsActive] = useState(false);
   const [name, setName] = useState("Wongani");
   useEffect(() => {
+    
     let interval;
-    if (timer > 0 && isActive) {
-      interval = setInterval(() => {
-        setTimer((preState) => {
-          return preState - 1;
-        });
-      }, 1000);
-    } else if (timer === 0) {
-      setTimer(0);
-    }
+    if (  isActive) {
+      if (timer > 0) {
+        interval = setInterval(() => {
+          setTimer((preState) => {
+            return preState - 1;
+          });
+        }, 1000);
+        
+      }
+    } 
 
     return () => {
       clearInterval(interval);
     };
   }, [isActive, timer]);
   const reset = () => {
-    setTimer(60)
-    setIsActive(false)
- }
+    setTimer(60);
+    setIsActive(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">timer</header>
-      <section className='container'>
-        <div className='btn'>
+      <header className="App-header">
+        <h1>Timer</h1>
+        <p>A simple countdown timer</p>
+      </header>
+      <section className="container">
+        <div className="btn">
           <button onClick={() => setIsActive(!isActive)}>Start</button>
           <button onClick={reset}>Reset</button>
         </div>
-      <div className='value'>
-        <h1>{timer !== 0 ? timer : name}</h1>
-      </div>
+        <div className="value">
+          {timer !== 0 ? <h2>{timer} </h2> : <h1>{name}</h1>}
+        </div>
       </section>
     </div>
   );
