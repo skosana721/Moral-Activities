5bc261d5532990a2ab08aa5d1e5b347433aaa32e
TCG <wonganiskosana@gmail.com>
2021-02-02 12:21:14 +0200

    added onchange to the input and created a method to handleChange

--- search-app/src/component/Form.js
+++ search-app/src/component/Form.js
@@ -1,31 +1,76 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-function Form () {
-    const [formInfo, setFormInfo] = useState({
-        name: '',
-        surname: '',
-        ID: '',
-        age: '',
-        place: '',
-        allergy:''
-    })
-  return (
-    <div>
+function Form() {
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    surname: "",
+    ID: "",
+    age: "",
+    place: "",
+    allergy: "",
+  });
+  const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormInfo({...formInfo,
+          [name]: value,
+        });
+    };
+    const { name, surname, ID, age, place, allergy } = formInfo;
+    return (
+        <div>
       <form>
-        <input type="text" name="name" id="" placeholder="Name" />
-        <input type="text" name="Surname" id="" placeholder="surname" />
-        <input type="number" name="ID" id="" placeholder="ID Number" />
-        <input type="number" name="age" id="" placeholder="Age" />
-        <input type="text" name="place" id="" placeholder="Place" />
+        <input
+          type="text"
+          name="name"
+          id=""
+          placeholder="Name"
+          onChange={handleChange}
+          value={name}
+          />
+        <input
+          type="text"
+          name="Surname"
+          id=""
+          placeholder="surname"
+          onChange={handleChange}
+          value={surname}
+          />
+        <input
+          type="number"
+          name="ID"
+          id=""
+          placeholder="ID Number"
+          onChange={handleChange}
+          value={ID}
+          />
+        <input
+          type="number"
+          name="age"
+          id=""
+          placeholder="Age"
+          onChange={handleChange}
+          value={age}
+          />
+        <input
+          type="text"
+          name="place"
+          id=""
+          placeholder="Place"
+          onChange={handleChange}
+          value={place}
+          />
         <textarea
-          name=""
+          name="allergy"
           id=""
           cols="25"
           rows="10"
           placeholder="Allergy"
-        ></textarea>
+          onChange={handleChange}
+          value={allergy}
+          ></textarea>
         <button type="submit">Submit</button>
-      </form>
+            </form>
+            {console.log('info', formInfo.name)}
     </div>
   );
 }
