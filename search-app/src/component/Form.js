1eda5537c15b493776a4c44d08b6e6f291168400
TCG <wonganiskosana@gmail.com>
2021-02-02 11:49:40 +0200

    created a form that has input for name, surname, age, place, id number, and textarea for allergy

--- search-app/src/component/Form.js
+++ search-app/src/component/Form.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <form>
+        <input type="text" name="name" id="" placeholder="name" />
+        <input type="text" name="surname" id="" placeholder="surname" />
+        <input type="number" name="ID" id="" placeholder="ID Number" />
+        <input type="number" name="age" id="" placeholder="Age" />
+        <input type="text" name="place" id="" placeholder="Place" />
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="allergy"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
