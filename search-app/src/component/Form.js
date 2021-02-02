900672857f1e33f967d18d199625a8246f39d564
TCG <wonganiskosana@gmail.com>
2021-02-02 13:18:15 +0200

    add style to form component

--- search-app/src/component/Form.js
+++ search-app/src/component/Form.js
@@ -10,73 +10,87 @@ function Form() {
     allergy: "",
   });
   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormInfo({...formInfo,
-          [name]: value,
-        });
+    const { name, value } = e.target;
+    setFormInfo({ ...formInfo, [name]: value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("info", formInfo);
-  }
-  
-    const { name, surname, ID, age, place, allergy } = formInfo;
-    return (
-        <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          id=""
-          placeholder="Name"
-          onChange={handleChange}
+  };
+
+  const { name, surname, ID, age, place, allergy } = formInfo;
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-control">
+          <input
+            type="text"
+            name="name"
+            id=""
+            placeholder="Name"
+            onChange={handleChange}
             value={name}
             required
           />
-        <input
-          type="text"
-          name="surname"
-          id=""
-          placeholder="surname"
-          onChange={handleChange}
-          value={surname}
-          required/>
-        <input
-          type="number"
-          name="ID"
-          id=""
-          placeholder="ID Number"
-          onChange={handleChange}
-          value={ID}
-          required/>
-        <input
-          type="number"
-          name="age"
-          id=""
-          placeholder="Age"
-          onChange={handleChange}
-          value={age}
-          required/>
-        <input
-          type="text"
-          name="place"
-          id=""
-          placeholder="Place"
-          onChange={handleChange}
-          value={place}
-          required/>
-        <textarea
-          name="allergy"
-          id=""
-          cols="25"
-          rows="10"
-          placeholder="Allergy"
-          onChange={handleChange}
-          value={allergy}
-          required></textarea>
+        </div>
+        <div className="form-control">
+          <input
+            type="text"
+            name="surname"
+            id=""
+            placeholder="surname"
+            onChange={handleChange}
+            value={surname}
+            required
+          />
+        </div>
+        <div className="form-control">
+          <input
+            type="number"
+            name="ID"
+            id=""
+            placeholder="ID Number"
+            onChange={handleChange}
+            value={ID}
+            required
+          />
+        </div>
+        <div className="form-control">
+          <input
+            type="number"
+            name="age"
+            id=""
+            placeholder="Age"
+            onChange={handleChange}
+            value={age}
+            required
+          />
+        </div>
+        <div className="form-control">
+          <input
+            type="text"
+            name="place"
+            id=""
+            placeholder="Place"
+            onChange={handleChange}
+            value={place}
+            required
+          />
+        </div>
+        <div className="form-control">
+          <textarea
+            name="allergy"
+            id=""
+            cols="25"
+            rows="10"
+            placeholder="Allergy"
+            onChange={handleChange}
+            value={allergy}
+            required
+          ></textarea>
+        </div>
         <button type="submit">Submit</button>
-            </form>
-            
+      </form>
     </div>
   );
 }
