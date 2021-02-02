ee650d1d7bfb347981cf5d35c82957288785f0be
TCG <wonganiskosana@gmail.com>
2021-02-02 10:32:23 +0200

    added 4 empty component files which are form, seachBar, students, and studentsList

---
+++ search-app/src/component/SeachBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SeachBar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SeachBar
---
+++ search-app/src/component/StudentsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Students from './Students'
+
+function List() {
+    return (
+        <div>
+            <Students/>
+        </div>
+    )
+}
+
+export default List
