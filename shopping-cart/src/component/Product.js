54a74754b45986c77cab6c21da3535d2d9e0ccb1
skosana721 <wonganiskosana@gmail.com>
2021-02-09 13:29:22 +0200

    installed material-ui icons and and added material-ui style to product component

--- shopping-cart/src/component/Product.js
+++ shopping-cart/src/component/Product.js
@@ -1,20 +1,46 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import { Paper } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 800,
+    height: 800,
+  },
+}));
 export const Product = () => {
   const products = useSelector((state) => state.products.products);
+  const classes = useStyles();
 
   return (
-    <div>
-      {products.map((product) => {
-        const { id, brand, model, img, price } = product;
-        return (
-          <div key={id}>
-            <h1>{brand}</h1>
-            <h2>{model}</h2>
-          </div>
-        );
-      })}
+    <div className={classes.root}>
+      <GridList
+        cellHeight="auto"
+        className={classes.gridList}
+        cols={3}
+        spacing={4}
+      >
+        {products.map((product) => (
+          <GridListTile key={product.id}>
+            <Paper variant="outlined" square>
+              <h2>{product.brand}</h2>
+              <h3>{product.model}</h3>
+              <Button variant="outlined">Add</Button>
+            </Paper>
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 };
