1430bb8f5deb5c568e1e8e4fb2d7e99b30649f1c
skosana721 <wonganiskosana@gmail.com>
2021-02-09 15:04:08 +0200

    passed the cart reducer to root reducer

--- shopping-cart/src/redux/rootReducer.js
+++ shopping-cart/src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { cartReducer } from "./cart/cartReducer";
 import { productReducer } from "./Product/productReducer";
 
 export const rootReducer = combineReducers({
   products: productReducer,
+  cart: cartReducer,
 });
