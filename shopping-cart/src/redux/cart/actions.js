0f390d9423e3c13e30d12249897a7456ef9f83eb
skosana721 <wonganiskosana@gmail.com>
2021-02-09 15:00:47 +0200

    added an action for addToCart and actionType of ADD_TO_CART

--- shopping-cart/src/redux/cart/actions.js
+++ shopping-cart/src/redux/cart/actions.js
@@ -1,7 +1,10 @@
+import { ADD_TO_CART } from "./actionTypes";
+
 export const addToCart = (data) => {
-    return dispatch => {
-        
-        dispatch({})
-    }
-}
-}
+  return (dispatch) => {
+    dispatch({
+      type: ADD_TO_CART,
+      payload: data,
+    });
+  };
+};
