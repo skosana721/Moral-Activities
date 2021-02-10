a423ce76ab09e168d0217e28d07c7dd5dc7777c5
skosana721 <wonganiskosana@gmail.com>
2021-02-10 13:28:50 +0200

    added a div with a key prop of "item.id"

--- shopping-cart/src/component/Cart.js
+++ shopping-cart/src/component/Cart.js
@@ -12,8 +12,13 @@ export const Cart = () => {
     <div>
       {cart.length !== 0 &&
         cart.map((item) => {
-          return <h1> {item.brand}</h1>;
+          return (
+            <div key={item.id}>
+              <h1> {item.brand}</h1>;
+            </div>
+          );
         })}
+
       <h2>Total: R {total}</h2>
       <Button
         onClick={() => dispatch(clearCart())}
