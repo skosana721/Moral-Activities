cad748dd49665c7d9cfc6015a8548b9654eedfbf
skosana721 <wonganiskosana@gmail.com>
2021-02-10 13:00:16 +0200

    imported a clear icon from material-ui/icons and added it to the button after textContent

--- shopping-cart/src/component/Cart.js
+++ shopping-cart/src/component/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { useSelector } from "react-redux";
+import ClearIcon from "@material-ui/icons/Clear";
 
 export const Cart = () => {
   const { cart, total } = useSelector((state) => state.cart);
@@ -12,7 +13,7 @@ export const Cart = () => {
           return <h1> {item.brand}</h1>;
         })}
       <h2>Total: R {total}</h2>
-      <Button variant="contained" color="secondary">
+      <Button variant="contained" color="secondary" endIcon={<ClearIcon />}>
         Clear
       </Button>
     </div>
