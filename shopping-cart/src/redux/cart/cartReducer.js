7a1891605fbf1a33c55f4ca0134f918c9c861957
skosana721 <wonganiskosana@gmail.com>
2021-02-10 12:23:02 +0200

    updates state when actionType ADD_TO_CART is dispatch from the product component and added total to state with initial value of zero

--- shopping-cart/src/redux/cart/cartReducer.js
+++ shopping-cart/src/redux/cart/cartReducer.js
@@ -1,8 +1,16 @@
+import { ADD_TO_CART } from "./actionTypes";
+
 const initialState = {
   cart: [],
+  total: "0",
 };
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      };
     default:
       return state;
   }
