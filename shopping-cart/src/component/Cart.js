9c3da378e5b9f0db7080ef5e2c4b8e4e756feb1f
skosana721 <wonganiskosana@gmail.com>
2021-02-10 12:13:34 +0200

    maped cart product from redux store

---
+++ shopping-cart/src/component/Cart.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export const Cart = () => {
+  const { cart, total } = useSelector((state) => state.cart);
+  console.log("cart", cart);
+  return (
+    <div>
+      {cart.length !== 0 &&
+        cart.map((item) => {
+          return <h1> {item.brand}</h1>;
+        })}
+      <h2>{total}</h2>
+    </div>
+  );
+};
