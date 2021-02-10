ecf3def7c3235fa471747df47384b2906ecf2cde
skosana721 <wonganiskosana@gmail.com>
2021-02-10 12:16:08 +0200

    import the cart component and render it

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Cart } from "./component/Cart";
 import { Product } from "./component/Product";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">Shopping Cart</header>
       <Product />
+      <Cart />
     </div>
   );
 }
