fd1218c682f44c4d2a47da75cf3a26fb9a0f1131
skosana721 <wonganiskosana@gmail.com>
2021-02-11 14:02:33 +0200

    added INCREAESE_AMOUNT actionType

--- shopping-cart/src/redux/cart/actionTypes.js
+++ shopping-cart/src/redux/cart/actionTypes.js
@@ -2,3 +2,4 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const CLEAR_CART = "CLEAR_CART";
 export const GET_TOTAL_AMOUNT = "GET_TOTAL_AMOUNT";
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
+export const INCREASE_AMOUNT = "INCREASE_AMOUNT";
