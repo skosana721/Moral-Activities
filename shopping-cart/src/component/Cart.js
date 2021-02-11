49fb49fa081950c01ba9d7971e6f215914599ca9
skosana721 <wonganiskosana@gmail.com>
2021-02-11 14:07:33 +0200

    added ExpendLessSharp icon from material-ui/icons and added onClick which dispatch inceaseAmount with an id as parameter

--- shopping-cart/src/component/Cart.js
+++ shopping-cart/src/component/Cart.js
@@ -6,8 +6,10 @@ import {
   clearCart,
   getTotalAmount,
   removeProduct,
+  increaseAmount,
 } from "../redux/cart/actions";
 import RemoveItem from "@material-ui/icons/RemoveShoppingCart";
+import AddIcon from "@material-ui/icons/ExpandLessSharp";
 
 export const Cart = () => {
   const { cart, total, amount } = useSelector((state) => state.cart);
@@ -23,10 +25,12 @@ export const Cart = () => {
       {amount}
       {cart.length !== 0 &&
         cart.map((item) => {
-          const { id, brand, model, price } = item;
+          const { id, brand, model, price, amount } = item;
           return (
             <div key={id}>
+              {amount}
               <h2> {brand}</h2>
+              <AddIcon onClick={() => dispatch(increaseAmount(id))} />
               <h3>{model}</h3>
               <h4>R {price}</h4>
               <RemoveItem onClick={() => dispatch(removeProduct(id))} />
