c9a63d0513ae903514ddb66e789e1176e8b8ceb8
skosana721 <wonganiskosana@gmail.com>
2021-02-12 10:36:36 +0200

    import BrowseRouter as router , route, switch from react-router-dom and repped app componter router and used the switch to switch between component which where pass as props in the route component

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import { Cart } from "./component/Cart";
 import { Product } from "./component/Product";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Shopping Cart</header>
-      <Product />
-      <Cart />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">Shopping Cart</header>
+        <Switch>
+          <Route path="/" exact component={Product} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
