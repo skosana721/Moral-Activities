a648f9e0e332f364ebf4e602252f3d2cf341ec2d
skosana721 <wonganiskosana@gmail.com>
2021-02-15 12:55:22 +0200

    added rootReducer to the store

--- reserve-park/src/redux/store/index.js
+++ reserve-park/src/redux/store/index.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { rootReducer } from "../rootReducer";
 
- const composeEnhancers =
-     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(,composeEnhancers(applyMiddleware(thunk)))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
