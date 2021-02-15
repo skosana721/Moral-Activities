9ff2c92aca16bb680f28e2864fbc1127901411ce
skosana721 <wonganiskosana@gmail.com>
2021-02-15 15:02:47 +0200

    used useSelector to display the number of male lions and female lions and also the total of lion from redux store but total of lions is initial zero

--- reserve-park/src/component/Lions.js
+++ reserve-park/src/component/Lions.js
@@ -2,9 +2,14 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function Lions() {
+  const lion = useSelector((state) => state.lion);
+
   return (
     <div>
       <h2>Lions</h2>
+      <h3>Male Lions: {lion.maleLions}</h3>
+      <h3>Female Lions: {lion.femaleLions}</h3>
+      <p>Total Number of Lion in reserver: {lion.totalLions}</p>
     </div>
   );
 }
