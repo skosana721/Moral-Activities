046af42cfcac9e1ff7e8ac21ecfbb6ac98cee0f1
skosana721 <wonganiskosana@gmail.com>
2021-02-15 12:58:00 +0200

    added files for actions, actionTypes and lionReducer

---
+++ reserve-park/src/redux/lion/lionReducer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const initialState = {
+  maleLions: 8,
+  femaleLions: 18,
+  totalLions: 0,
+};
+
+export const lionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
