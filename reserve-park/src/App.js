46919f4ba8e5c334c8d526e12302b587335bd959
skosana721 <wonganiskosana@gmail.com>
2021-02-15 13:37:08 +0200

    rendered the Rhinos componet to the app

--- reserve-park/src/App.js
+++ reserve-park/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Lions from "./component/Lions";
+import Rhinos from "./component/Rhinos";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>Reserver Park</h1>
       </header>
       <Lions />
+      <Rhinos />
     </div>
   );
 }
