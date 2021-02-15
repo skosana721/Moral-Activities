f3d0ba5e9f3c238d6d3ef2d9afadff15dc11e5db
skosana721 <wonganiskosana@gmail.com>
2021-02-15 13:33:49 +0200

    changed folder name

--- reserve-park/src/compoenent/Lions.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Lions() {
-  return <div></div>;
-}
-
-export default Lions;
--- reserve-park/src/compoenent/Rhinos.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Rhinos() {
-  return <div></div>;
-}
-
-export default Rhinos;
