469d7abf8e42d5f0d63f07b320ae38a4e650c088
skosana721 <wonganiskosana@gmail.com>
2021-02-15 13:06:33 +0200

    created the initialState for the number of female , male and total number of rhinos

--- reserve-park/src/redux/rootReducer.js
+++ reserve-park/src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducer } from "react-redux";
 import { lionReducer } from "./lion/lionReducer";
+import { rhinoReducer } from "./rhino/rhinoReducer";
 
 export const rootReducer = combineReducer({
   lion: lionReducer,
+  rhino: rhinoReducer,
 });
