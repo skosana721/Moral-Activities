24c60b69a1a2672d514bc55b07bffb73a6aec6c3
skosana721 <wonganiskosana@gmail.com>
2021-02-15 13:07:32 +0200

    added files for actions, actionTypes and rhinoReducer

---
+++ reserve-park/src/redux/rhino/rhinoReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  maleRhinos: 11,
+  femaleRhinos: 23,
+  totalRhino: 34,
+};
+
+export const rhinoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
