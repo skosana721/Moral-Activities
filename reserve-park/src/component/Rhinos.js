db89ce264fa6ec03fb27c7dbbd4c129b1fbfdd64
skosana721 <wonganiskosana@gmail.com>
2021-02-15 15:05:14 +0200

    used useSelector to diplay the number of male rhinos and female rhinos and also total number of the rhinos from redux store

--- reserve-park/src/component/Rhinos.js
+++ reserve-park/src/component/Rhinos.js
@@ -2,7 +2,14 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function Rhinos() {
-  return <div></div>;
+  const rhino = useSelector((state) => state.rhino);
+  return (
+    <div>
+      <h2>Rhinos</h2>
+      <h3>Male Rhinos: {rhino.maleRhinos}</h3>
+      <h3>Female Rhinos: {rhino.femaleRhinos}</h3>
+    </div>
+  );
 }
 
 export default Rhinos;
