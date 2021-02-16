da578c99568d95fc9cc2dda7e93b3bc26ea07f10
skosana721 <wonganiskosana@gmail.com>
2021-02-16 10:45:34 +0200

    created a button with onClick function that has a method reduceMaleNum the method has an if statement that only calls dispatch if the number of male lions is greater than 0

--- reserve-park/src/component/Lions.js
+++ reserve-park/src/component/Lions.js
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getTotal } from "../redux/lion/actions";
+import { getTotal, reduceMaleNumber } from "../redux/lion/actions";
 
 function Lions() {
   const dispatch = useDispatch();
   const lion = useSelector((state) => state.lion);
   useEffect(() => {
     dispatch(getTotal());
-  }, [lion.totalLions, dispatch]);
-
+  }, [lion.maleLions, dispatch]);
+  const reduceMaleNum = () => {
+    if (lion.maleLions > 0) {
+      dispatch(reduceMaleNumber());
+    }
+  };
   return (
     <div>
       <h2>Lions</h2>
       <h3>Male Lions: {lion.maleLions}</h3>
+      <button onClick={reduceMaleNum}>-</button>
       <h3>Female Lions: {lion.femaleLions}</h3>
       <p>Total Number of Lion in reserver: {lion.totalLions}</p>
     </div>
