b4a0d36549c3396200dbf914382228edf0b525c5
skosana721 <wonganiskosana@gmail.com>
2021-02-16 11:29:30 +0200

    added case for REDUCE_FEMALE_NUMBER which decrements the number female lions in the state everytime the actionType is dipatch

--- reserve-park/src/redux/lion/lionReducer.js
+++ reserve-park/src/redux/lion/lionReducer.js
@@ -1,4 +1,8 @@
-import { GET_TOTAL, REDUCE_MALE_NUMBER } from "./actionTypes";
+import {
+  GET_TOTAL,
+  REDUCE_FEMALE_NUMBER,
+  REDUCE_MALE_NUMBER,
+} from "./actionTypes";
 
 const initialState = {
   maleLions: 8,
@@ -12,6 +16,11 @@ export const lionReducer = (state = initialState, action) => {
       return {
         ...state,
         maleLions: state.maleLions - 1,
+      };
+    case REDUCE_FEMALE_NUMBER:
+      return {
+        ...state,
+        femaleLions: state.femaleLions - 1,
       };
     case GET_TOTAL:
       let total = state.femaleLions + state.maleLions;
