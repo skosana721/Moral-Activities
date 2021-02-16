33913da3c1a40f100fff9264c08bcf9f3995607c
skosana721 <wonganiskosana@gmail.com>
2021-02-16 11:54:03 +0200

    added case for DECREMENT_MALE_RHINOS  which decremtn the number of male rhinos by 1

--- reserve-park/src/redux/rhino/rhinoReducer.js
+++ reserve-park/src/redux/rhino/rhinoReducer.js
@@ -1,11 +1,18 @@
+import { DECREMENT_MALE_RHINOS } from "./actionTypes";
+
 const initialState = {
   maleRhinos: 11,
   femaleRhinos: 23,
-  totalRhino: 0,
+  totalRhinos: 0,
 };
 
 export const rhinoReducer = (state = initialState, action) => {
   switch (action.type) {
+    case DECREMENT_MALE_RHINOS:
+      return {
+        ...state,
+        maleRhinos: state.maleRhinos - 1,
+      };
     default:
       return state;
   }
