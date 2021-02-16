b3a15dfa50a5ae627f373b556fabf7646585cced
skosana721 <wonganiskosana@gmail.com>
2021-02-16 10:52:07 +0200

    added a case for REDUCE_MALE_NUMBER which decrements the number of maleLions every time this case is called

--- reserve-park/src/redux/lion/lionReducer.js
+++ reserve-park/src/redux/lion/lionReducer.js
@@ -1,4 +1,4 @@
-import { GET_TOTAL } from "./actionTypes";
+import { GET_TOTAL, REDUCE_MALE_NUMBER } from "./actionTypes";
 
 const initialState = {
   maleLions: 8,
@@ -8,8 +8,14 @@ const initialState = {
 
 export const lionReducer = (state = initialState, action) => {
   switch (action.type) {
+    case REDUCE_MALE_NUMBER:
+      return {
+        ...state,
+        maleLions: state.maleLions - 1,
+      };
     case GET_TOTAL:
       let total = state.femaleLions + state.maleLions;
+
       return {
         ...state,
         totalLions: total,
