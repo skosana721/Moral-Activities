8e35041836db6aea48eefe72091bf9a510f13535
skosana721 <wonganiskosana@gmail.com>
2021-02-16 13:32:33 +0200

    created action of getRhinoTotal which dispatch an actionType of GET_RHINO_TOTAL

--- reserve-park/src/redux/rhino/actions.js
+++ reserve-park/src/redux/rhino/actions.js
@@ -1,4 +1,8 @@
-import { DECREMENT_FEMALE_RHINOS, DECREMENT_MALE_RHINOS } from "./actionTypes";
+import {
+  DECREMENT_FEMALE_RHINOS,
+  DECREMENT_MALE_RHINOS,
+  GET_RHINO_TOTAL,
+} from "./actionTypes";
 
 export const decrementMaleRhinos = () => {
   return (dispatch) => {
@@ -8,5 +12,10 @@ export const decrementMaleRhinos = () => {
 export const decrementFemaleRhinos = () => {
   return (dispatch) => {
     dispatch({ type: DECREMENT_FEMALE_RHINOS });
+  };
+};
+export const getRhinoTotal = () => {
+  return (dispatch) => {
+    dispatch({ type: GET_RHINO_TOTAL });
   };
 };
