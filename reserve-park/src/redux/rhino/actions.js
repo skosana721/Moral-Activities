ecf3c7a8e3381267cff30d34faa8e76011ba6889
skosana721 <wonganiskosana@gmail.com>
2021-02-16 12:14:48 +0200

    created an action of decrementFamaleRhinos which dispatch actionTypes of DECREMENT_FEMALE_RHINOS

--- reserve-park/src/redux/rhino/actions.js
+++ reserve-park/src/redux/rhino/actions.js
@@ -1,7 +1,12 @@
-import { DECREMENT_MALE_RHINOS } from "./actionTypes";
+import { DECREMENT_FEMALE_RHINOS, DECREMENT_MALE_RHINOS } from "./actionTypes";
 
 export const decrementMaleRhinos = () => {
   return (dispatch) => {
     dispatch({ type: DECREMENT_MALE_RHINOS });
+  };
+};
+export const decrementFemaleRhinos = () => {
+  return (dispatch) => {
+    dispatch({ type: DECREMENT_FEMALE_RHINOS });
   };
 };
