cdf0ac21a10373795f63262abb926a9c394bc7c6
skosana721 <wonganiskosana@gmail.com>
2021-02-16 11:47:35 +0200

    created a button with onClick that has a method decrementMaleNuber and that method has and if statement that checks if the number of maleRhins greater than zero which for everytime that is true it dispatch the actions decremtnMaleRhinos

--- reserve-park/src/component/Rhinos.js
+++ reserve-park/src/component/Rhinos.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { decrementMaleRhinos } from "../redux/rhino/actions";
 
 function Rhinos() {
   const rhino = useSelector((state) => state.rhino);
+  const dispatch = useDispatch();
+  const decrementMaleNumber = () => {
+    if (rhino.maleRhinos > 0) {
+      dispatch(decrementMaleRhinos());
+    }
+  };
   return (
     <div>
       <h2>Rhinos</h2>
       <h3>Male Rhinos: {rhino.maleRhinos}</h3>
+      <button onClick={decrementMaleNumber}>dead</button>
       <h3>Female Rhinos: {rhino.femaleRhinos}</h3>
+      <p>Total Number Of Rhinos reserver: {rhino.totalRhinos}</p>
     </div>
   );
 }
