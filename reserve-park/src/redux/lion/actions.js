63ef77afb9980d8358759fe47fc272c0522c0fb8
skosana721 <wonganiskosana@gmail.com>
2021-02-16 11:27:21 +0200

    added action for reduceFemaleNumber which dipsatch an actionType for REDUCE_FEMALE_NUMBER

--- reserve-park/src/redux/lion/actions.js
+++ reserve-park/src/redux/lion/actions.js
@@ -1,12 +1,21 @@
-import { GET_TOTAL, REDUCE_MALE_NUMBER } from "./actionTypes";
+import {
+  GET_TOTAL,
+  REDUCE_FEMALE_NUMBER,
+  REDUCE_MALE_NUMBER,
+} from "./actionTypes";
+
 export const getTotal = () => {
   return (dispatch) => {
     dispatch({ type: GET_TOTAL });
   };
 };
-
 export const reduceMaleNumber = () => {
   return (dispatch) => {
     dispatch({ type: REDUCE_MALE_NUMBER });
+  };
+};
+export const reduceFemaleNumber = () => {
+  return (dispatch) => {
+    dispatch({ type: REDUCE_FEMALE_NUMBER });
   };
 };
