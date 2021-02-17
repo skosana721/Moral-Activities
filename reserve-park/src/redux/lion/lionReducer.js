e24d55ceb454d893d9b600a2650d9a71f6197300
skosana721 <wonganiskosana@gmail.com>
2021-02-17 09:41:10 +0200

    added a case for SAVE_MALE_TOTAL which updates maleLions with an action.payload

--- reserve-park/src/redux/lion/lionReducer.js
+++ reserve-park/src/redux/lion/lionReducer.js
@@ -2,16 +2,22 @@ import {
   GET_TOTAL,
   REDUCE_FEMALE_NUMBER,
   REDUCE_MALE_NUMBER,
+  SAVE_MALE_TOTAL,
 } from "./actionTypes";
 
 const initialState = {
-  maleLions: 8,
-  femaleLions: 18,
+  maleLions: 0,
+  femaleLions: 0,
   totalLions: 0,
 };
 
 export const lionReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SAVE_MALE_TOTAL:
+      return {
+        ...state,
+        maleLions: action.payload,
+      };
     case REDUCE_MALE_NUMBER:
       return {
         ...state,
