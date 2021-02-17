3c9b1996fd552f568090a76f4d6ace66de447e8f
skosana721 <wonganiskosana@gmail.com>
2021-02-17 11:01:56 +0200

    added number and parseFloat to convert string to numbers

--- reserve-park/src/redux/rhino/rhinoReducer.js
+++ reserve-park/src/redux/rhino/rhinoReducer.js
@@ -17,12 +17,12 @@ export const rhinoReducer = (state = initialState, action) => {
     case SAVE_MALE_RHINO_TOTAL:
       return {
         ...state,
-        maleRhinos: action.payload,
+        maleRhinos: Number(action.payload),
       };
     case SAVE_FEMALE_RHINO_TOTAL:
       return {
         ...state,
-        femaleRhinos: action.payload,
+        femaleRhinos: Number(action.payload),
       };
     case DECREMENT_MALE_RHINOS:
       return {
@@ -36,6 +36,7 @@ export const rhinoReducer = (state = initialState, action) => {
       };
     case GET_RHINO_TOTAL:
       let total = state.femaleRhinos + state.maleRhinos;
+      console.log(total);
       return {
         ...state,
         totalRhinos: total,
