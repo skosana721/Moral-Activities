f1bf196ac7665cf8a7b172143a7d77beee36f545
skosana721 <wonganiskosana@gmail.com>
2021-02-22 15:06:07 +0200

    imported registerFormReducer and added it to rootReducer

--- car-dealer/src/redux/rootReducer.js
+++ car-dealer/src/redux/rootReducer.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import { registerFormReducer } from "./registerForm/registerFormReducer";
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  register: registerFormReducer,
+});
