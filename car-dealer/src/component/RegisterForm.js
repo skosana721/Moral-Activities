5a13b09014f543a552b55dcfcae809500d01de71
skosana721 <wonganiskosana@gmail.com>
2021-02-22 11:24:13 +0200

    created a form that has inputs for car brand, car model , price contact details , location , file upload and submit button

--- car-dealer/src/component/RegisterForm.js
+++ car-dealer/src/component/RegisterForm.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function RegisterForm() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          name="brand"
+          placeholder="Enter car brand (e.g VW)"
+        />
+        <input
+          type="text"
+          name="model"
+          placeholder="Enter car model (e.g Polo)"
+        />
+        <input type="number" name="" placeholder="" />
+        <input type="number" name="price" placeholder="Price" />
+        <input type="number" name="contact" placeholder="Contact Details" />
+        <input type="text" name="location" placeholder="Enter Location" />
+        <input
+          type="file"
+          accept="image/gif, image/jpeg, image/png"
+          name="image"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterForm;
