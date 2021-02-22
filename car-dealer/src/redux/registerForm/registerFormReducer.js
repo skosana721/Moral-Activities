347ba80b6101c05f91e761ee427a60c0edb8919e
skosana721 <wonganiskosana@gmail.com>
2021-02-22 15:12:25 +0200

    created a registerFormReducer which has initalState of registerForm as an emppty array

---
+++ car-dealer/src/redux/registerForm/registerFormReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  registerForm: [],
+};
+
+export const registerFormReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
