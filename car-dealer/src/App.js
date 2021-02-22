4e344fa5754e9206a143876b16aa7d3caf5b2fc4
skosana721 <wonganiskosana@gmail.com>
2021-02-22 10:38:43 +0200

    rendered registerForm component

--- car-dealer/src/App.js
+++ car-dealer/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import RegisterForm from "./component/RegisterForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <RegisterForm />
     </div>
   );
 }
