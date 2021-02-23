e5da8cdc7c72beef156e97ca2aa8581e84a71167
skosana721 <wonganiskosana@gmail.com>
2021-02-23 09:52:28 +0200

    added the viewCarReducer to the rootReducer

--- car-dealer/src/redux/rootReducer.js
+++ car-dealer/src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { registerFormReducer } from "./registerForm/registerFormReducer";
+import { viewCarReducer } from "./viewCar/viewCarReducer";
 
 export const rootReducer = combineReducers({
   register: registerFormReducer,
+  view: viewCarReducer,
 });
