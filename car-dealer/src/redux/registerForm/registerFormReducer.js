ab97ee2aaa734f8494202d15dad6c5e105a70733
skosana721 <wonganiskosana@gmail.com>
2021-02-23 09:31:38 +0200

    added a case for SAVE_REGISTER_DATA which recieves data is a payload and updates registerForm

--- car-dealer/src/redux/registerForm/registerFormReducer.js
+++ car-dealer/src/redux/registerForm/registerFormReducer.js
@@ -1,9 +1,16 @@
+import { SAVE_REGISTER_DATA } from "./actionTypes";
+
 const initialState = {
   registerForm: [],
 };
 
 export const registerFormReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SAVE_REGISTER_DATA:
+      return {
+        ...state,
+        registerForm: [...state.registerForm, action.payload],
+      };
     default:
       return state;
   }
