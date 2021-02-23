602321c0bb092b7e0835d381b18ef921a26e48be
skosana721 <wonganiskosana@gmail.com>
2021-02-23 15:20:21 +0200

    imported persistReducer from redux-persist and Storage from "redux-persist/lib/storage" and created const variable persistConfig  key of key and value of root and has storage and whitelist as array which takes state that needs to be persisted

--- car-dealer/src/redux/rootReducer.js
+++ car-dealer/src/redux/rootReducer.js
@@ -1,8 +1,17 @@
 import { combineReducers } from "redux";
 import { registerFormReducer } from "./registerForm/registerFormReducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { viewCarReducer } from "./viewCar/viewCarReducer";
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["register"],
+};
+const rootReducer = combineReducers({
   register: registerFormReducer,
   view: viewCarReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
