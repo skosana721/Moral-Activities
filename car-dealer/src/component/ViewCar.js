fe18944d32621305578478d1c765e40a4e13d47e
skosana721 <wonganiskosana@gmail.com>
2021-02-23 10:02:47 +0200

    imported useSelector to access the rigester state and assigned it to const variable of cars and mapped it

--- car-dealer/src/component/ViewCar.js
+++ car-dealer/src/component/ViewCar.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function ViewCar() {
-  return <div></div>;
+  const cars = useSelector((state) => state.register.registerForm);
+  console.log(cars);
+  return (
+    <div>
+      {cars.map((car) => {
+        return (
+          <div key={car.id}>
+            <h2>{car.brand}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ViewCar;
