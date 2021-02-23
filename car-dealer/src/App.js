45300b9984a5cb0bc87eebc0adecf4c772b4435c
skosana721 <wonganiskosana@gmail.com>
2021-02-23 09:42:59 +0200

    rendered the ViewCar component

--- car-dealer/src/App.js
+++ car-dealer/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import RegisterForm from "./component/RegisterForm";
+import ViewCar from "./component/ViewCar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <RegisterForm />
+      <ViewCar />
     </div>
   );
 }
