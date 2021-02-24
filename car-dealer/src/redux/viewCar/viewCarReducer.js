eea32f21f3f539db1577f443939fe9f761b8f444
skosana721 <wonganiskosana@gmail.com>
2021-02-24 16:16:34 +0200

    added case for selected car which updates state with a action.payload

--- car-dealer/src/redux/viewCar/viewCarReducer.js
+++ car-dealer/src/redux/viewCar/viewCarReducer.js
@@ -1,8 +1,15 @@
+import { SELECTED_CAR } from "./actionTypes";
+
 const initialState = {
-  ViewCar: [],
+  viewCars: [],
 };
 export const viewCarReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SELECTED_CAR:
+      return {
+        ...state,
+        viewCars: [...state.viewCars, action.payload],
+      };
     default:
       return state;
   }
