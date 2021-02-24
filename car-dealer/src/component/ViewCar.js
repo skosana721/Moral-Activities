1f4ef5b2dbf9334a40dee12e03f8dad50b34cc6b
skosana721 <wonganiskosana@gmail.com>
2021-02-24 10:25:44 +0200

    in the map function displaying car.model, car.price, car.contact and also car.locatioin

--- car-dealer/src/component/ViewCar.js
+++ car-dealer/src/component/ViewCar.js
@@ -3,13 +3,17 @@ import { useSelector } from "react-redux";
 
 function ViewCar() {
   const cars = useSelector((state) => state.register.registerForm);
-
+  console.log(cars);
   return (
     <div>
       {cars.map((car) => {
         return (
           <div key={car.id}>
             <h2>{car.brand}</h2>
+            <h2>{car.model}</h2>
+            <h2>R{car.price}</h2>
+            <h3>{car.contact}</h3>
+            <h3>{car.location}</h3>
           </div>
         );
       })}
