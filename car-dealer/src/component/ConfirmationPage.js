7afd5561b24abb0ded75862e8a91518333820d93
skosana721 <wonganiskosana@gmail.com>
2021-02-25 09:33:55 +0200

    imported useSelector to access viewCars state as a varialbe of const info  and mapped it

--- car-dealer/src/component/ConfirmationPage.js
+++ car-dealer/src/component/ConfirmationPage.js
@@ -1,5 +1,18 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 export default function ConfirmationPage() {
-  return <div></div>;
+  const info = useSelector((state) => state.view.viewCars);
+  console.log(info);
+  return (
+    <div>
+      {info.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.brand}</h2>
+          </div>
+        );
+      })}
+      <h2>Confirmation</h2>
+    </div>
+  );
 }
