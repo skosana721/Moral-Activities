c3e05151debd4f603c52171c1fd3694e45dcb2ec
skosana721 <wonganiskosana@gmail.com>
2021-02-25 09:55:57 +0200

    imported Broswer as Router , Route , Switch from react-router-dom and wrapped all component in app.js in Router which wraps Switch component and added route the components

--- car-dealer/src/App.js
+++ car-dealer/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ConfirmationPage from "./component/ConfirmationPage";
 import RegisterForm from "./component/RegisterForm";
 import ViewCar from "./component/ViewCar";
@@ -7,10 +8,14 @@ import ViewCar from "./component/ViewCar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <RegisterForm />
-      <ViewCar />
-      <ConfirmationPage />
+      <Router>
+        <header className="App-header"></header>
+        <Switch>
+          <Route path="/" exact component={RegisterForm} />
+          <Route path="/viewCar" component={ViewCar} />
+          <Route path="/confirmationPage" component={ConfirmationPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
