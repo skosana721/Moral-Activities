e68390cb05b6935b8823d16453c0453ea921fabe
skosana721 <wonganiskosana@gmail.com>
2021-02-26 12:19:42 +0200

    created ConfirmationForm component and moved the form from confirmationPage to  the confirmationForm

---
+++ car-dealer/src/component/ConfirmationForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ConfirmationForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <br />
+        <input type="text" />
+        <br />
+        <input type="text" name="" />
+        <br />
+        <select name="" id="">
+          <option value="">Select payment</option>
+          <option value="">Cash</option>
+          <option value="">Credit</option>
+        </select>
+        <br />
+      </form>
+    </div>
+  );
+}
+
+export default ConfirmationForm;
