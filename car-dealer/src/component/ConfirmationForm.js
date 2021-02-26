c473eadab3525643fe10aa5c1146e6678dc6ff74
skosana721 <wonganiskosana@gmail.com>
2021-02-26 12:31:44 +0200

    added other input and submit button

--- car-dealer/src/component/ConfirmationForm.js
+++ car-dealer/src/component/ConfirmationForm.js
@@ -1,14 +1,21 @@
 import React from "react";
-
+import { Button } from "reactstrap";
 function ConfirmationForm() {
   return (
     <div>
       <form>
-        <input type="text" />
+        <input type="text" name="name" placeholder="Enter name" />
         <br />
-        <input type="text" />
+        <input type="text" name="lastName" placeholder="Enter last name" />
         <br />
-        <input type="text" name="" />
+        <input
+          type="number"
+          name="contact"
+          id=""
+          placeholder="Enter contact details"
+        />
+        <br />
+        <input type="text" name="location" placeholder="Enter location" />
         <br />
         <select name="" id="">
           <option value="">Select payment</option>
@@ -16,6 +23,9 @@ function ConfirmationForm() {
           <option value="">Credit</option>
         </select>
         <br />
+        <Button color="primary" type="submit">
+          Submit
+        </Button>
       </form>
     </div>
   );
