fc725ce3ddd116e029f3ef0bc21b68cd3c75dda1
skosana721 <wonganiskosana@gmail.com>
2021-02-26 11:57:55 +0200

    imported useState and useEffect and created state of car and imported useParams and deconstructed it and used useEffect created a const variable which find an object that match useParams id updated the it the variable and displayed it

--- car-dealer/src/component/ConfirmationPage.js
+++ car-dealer/src/component/ConfirmationPage.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { useParams } from "react-router-dom";
+
 export default function ConfirmationPage() {
+  const [car, setCar] = useState("");
   const info = useSelector((state) => state.view.viewCars);
+  const { id } = useParams();
+  useEffect(() => {
+    const newCar = info.find((car) => car.id === id);
+    setCar(newCar);
+  }, []);
 
   return (
     <div>
-      {info.map((item) => {
-        return (
-          <div key={item.id}>
-            <h2>{item.brand}</h2>
-          </div>
-        );
-      })}
+      <h2>Confirmation Purchase</h2>
+
+      <Card
+        body
+        color="info"
+        style={{
+          color: "white",
+          margin: "0 35% ",
+        }}
+      >
+        <CardBody>
+          <CardTitle tag="h3">{car.brand}</CardTitle>
+          <CardSubtitle tag="h5">{car.model}</CardSubtitle>
+        </CardBody>
+        <CardBody>
+          <CardText tag="h6">R {car.price}</CardText>
+          <CardText tag="h6">{car.contact}</CardText>
+          <CardText tag="h6">{car.location}</CardText>
+        </CardBody>
+      </Card>
+
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" name="" />
+      </form>
     </div>
   );
 }
