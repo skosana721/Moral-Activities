dc8d4ca7d6e42eb5a2e371129e0959a729077317
skosana721 <wonganiskosana@gmail.com>
2021-03-01 11:49:38 +0200

    removed the header tag and rendered the Navbar component

--- car-dealer/src/App.js
+++ car-dealer/src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ConfirmationPage from "./component/ConfirmationPage";
 import RegisterForm from "./component/RegisterForm";
 import ViewCar from "./component/ViewCar";
+import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <NarBar />
+        <NavBar />
         <Switch>
           <Route path="/" exact component={RegisterForm} />
           <Route path="/viewCar" component={ViewCar} />
