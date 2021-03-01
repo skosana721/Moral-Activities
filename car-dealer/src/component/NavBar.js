78384cc0b079a8f43011f0dd2a8eec25639d6393
skosana721 <wonganiskosana@gmail.com>
2021-03-01 11:50:40 +0200

    created a navBar with a toggle with navigate to other pages

--- car-dealer/src/component/NavBar.js
+++ car-dealer/src/component/NavBar.js
@@ -10,7 +10,27 @@ import {
 } from "reactstrap";
 
 function NavBar() {
-  return <div></div>;
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+  return (
+    <div>
+      <Navbar light>
+        <NavbarBrand>Automotive</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavLink href="/viewCar">View Cars</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar;
