d71c77e933b7d3261c252f5391e5cfaf9c9b7eb1
skosana721 <wonganiskosana@gmail.com>
2021-03-04 12:46:09 +0200

    used useSelectort to access the register state and access modelContent and pass it to h2

--- car-dealer/src/component/Model.js
+++ car-dealer/src/component/Model.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Model() {
+  const modelContent = useSelector((state) => state.register.modelContent);
+  console.log(modelContent);
   return (
     <div>
-      <h2>Form submitted successfully</h2>
+      <h2>{modelContent}</h2>
     </div>
   );
 }
