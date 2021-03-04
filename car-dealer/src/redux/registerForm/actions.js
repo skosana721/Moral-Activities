571ff648ae230d24c8fcd90dc78dc8c24550b4e6
skosana721 <wonganiskosana@gmail.com>
2021-03-04 14:07:56 +0200

    created an action for closeModel

--- car-dealer/src/redux/registerForm/actions.js
+++ car-dealer/src/redux/registerForm/actions.js
@@ -1,7 +1,13 @@
-import { SAVE_REGISTER_DATA } from "./actionTypes";
+import { CLOSE_MODEL, SAVE_REGISTER_DATA } from "./actionTypes";
 
 export const saveRegisterData = (data) => {
   return (dispatch) => {
     dispatch({ type: SAVE_REGISTER_DATA, payload: data });
+  };
+};
+
+export const closeModel = () => {
+  return (dispatch) => {
+    dispatch({ type: CLOSE_MODEL });
   };
 };
