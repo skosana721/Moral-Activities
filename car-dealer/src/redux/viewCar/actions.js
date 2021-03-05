6feff3aa66ba8bcb60d71911ada33eec425e2199
skosana721 <wonganiskosana@gmail.com>
2021-03-05 12:28:56 +0200

    added an actions of purchaseCar which dispatch actionTypes of PURCHASE_CAR

--- car-dealer/src/redux/viewCar/actions.js
+++ car-dealer/src/redux/viewCar/actions.js
@@ -1,6 +1,11 @@
-import { SELECTED_CAR } from "./actionTypes";
+import { PURCHASED_CAR, SELECTED_CAR } from "./actionTypes";
 export const selectedCar = (data) => {
   return (dispatch) => {
     dispatch({ type: SELECTED_CAR, payload: data });
+  };
+};
+export const purchasedCar = () => {
+  return (dispatch) => {
+    dispatch({ type: PURCHASED_CAR });
   };
 };
