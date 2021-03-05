8cc1b2c4ee95b0f251bcaaa1c4ac035978de8905
skosana721 <wonganiskosana@gmail.com>
2021-03-05 12:27:45 +0200

    imported view state and created an ternary operator

--- car-dealer/src/component/ConfirmationPage.js
+++ car-dealer/src/component/ConfirmationPage.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import ConfirmationForm from "./ConfirmationForm";
 import SelectedCar from "./SelectedCar";
 
 export default function ConfirmationPage() {
+  const car = useSelector((state) => state.view.viewCars);
   return (
     <div className="confirm-page">
-      <h2>Confirm Purchase</h2>
-      <SelectedCar />
-      <ConfirmationForm />
+      {car.length !== 0 ? (
+        <div>
+          <h2>Confirm Purchase</h2>
+          <SelectedCar />
+          <ConfirmationForm />
+        </div>
+      ) : (
+        <div>
+          <h3>Your purchase is being processed...</h3>
+        </div>
+      )}
     </div>
   );
 }
